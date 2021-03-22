6c5eaef1ba5d8c0fdd7031a0b1e14e52c7f3a73d
Banghyungjin <Banghyungjin@gmail.com>
2021-03-22 23:41:45 +0900

    코드 이름변경
    영어 -> 한글 코드 이름 변경
    2020-03-22

---
+++ 프로그래머스/level_1/20_문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+    public static String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        char[] text = new char[s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            text[i] = s.charAt(i);
+        }
+        char temp;
+        for(int i = 0; i < s.length() - 1; i++) {
+            for(int j = i + 1; j < s.length(); j++) {
+                if(text[i] < text[j]) {
+                    temp = text[j];
+                    text[j] = text[i];
+                    text[i] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < s.length(); i++) {
+            answer.append(text[i]);
+        }
+        return answer.toString();
+    }
+    public static void main(String[] args) {
+	    String s = "Zbcdefg";
+	    if (solution(s).equals("gfedcbZ")) {
+	        System.out.println("True");
+        }
+	    else {
+            System.out.println("False");
+        }
+    }
+}
