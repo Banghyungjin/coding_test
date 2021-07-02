b83b9caa23c225d7f6cc4e3f7081e3899fb30802
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-02 11:59:15 +0900

    2021-07-02_01
    백준 2108 추가

--- 백준/banghyungjin_1181.py
+++ 백준/banghyungjin_1181.py
@@ -1,13 +1,9 @@
 import sys
 
-length_of_list = int(sys.stdin.readline().split()[0])   # 읽어올 리스트의 길이
-strings = []                                            # 리스트
+length_of_list = int(sys.stdin.readline().split()[0])                           # 읽어올 리스트의 길이
+strings = [sys.stdin.readline().split()[0] for _ in range(length_of_list)]      # 리스트
+strings = list(set(strings))                                                    # 중복값 제거
+strings.sort(key=lambda x: (len(x), x))                                         # 정렬 (1번째는 글자 순, 2번째는 사전 순)
 
-for i in range(length_of_list):                         # 리스트 길이 만큼
-    strings.append(sys.stdin.readline().split()[0])     # 내용을 읽어 리스트에 저장
-    
-strings = list(set(strings))                            # 중복값 제거
-strings.sort(key=lambda x: (len(x), x))                 # 정렬 (1번째는 글자 순, 2번째는 사전 순)
-
-for i in strings:                                       # 정답 출력
+for i in strings:                                                               # 정답 출력
     print(i)
