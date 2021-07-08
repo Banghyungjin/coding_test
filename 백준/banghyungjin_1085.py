ee29e3f1518e8f75a493ce3906567d708a2f40cb
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-08 10:39:36 +0900

    update 1085

--- 백준/banghyungjin_1085.py
+++ 백준/banghyungjin_1085.py
@@ -1,5 +1,6 @@
 import sys
 
-coords = list(map(int, sys.stdin.readline().split()[0]))
-print(coords)
-
+coords = list(map(int, sys.stdin.readline().split()))
+coords[2] -= coords[0]
+coords[3] -= coords[1]
+print(min(coords))
